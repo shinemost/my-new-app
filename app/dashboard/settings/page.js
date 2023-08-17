69b2a1b4fd70f554afac28422a368034ca0e166f
fuhaijing <fuhaijing@gotion.com.cn>
2023-08-17 11:22:01 +0800

    学习app router添加测试页面

---
+++ app/dashboard/settings/page.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: fuhaijing fuhaijing@gotion.com.cn
+ * @Date: 2023-08-17 11:16:14
+ * @LastEditors: fuhaijing fuhaijing@gotion.com.cn
+ * @LastEditTime: 2023-08-17 11:17:19
+ * @FilePath: \my-new-app\app\dashboard\settings\page.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+function SettingPage () {
+  return <div>
+    <h1>this is a settings page</h1>
+  </div>
+}
+
+export default SettingPage;
